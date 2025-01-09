1fcc70c516fa5ed0495cc8dad84c1ec68b5d3718
crazyman222214 <schadejr617@gmail.com>
2025-01-08 21:29:04 -0700

    Cellular Automaton

---
+++ Source/Cell.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+template<typename T>
+class Cells
+{
+public:
+
+	Cells(int width, int height) : m_height{ height }, m_width{ width }
+	{
+		m_data.resize(width * height);
+	}
+
+	const T& Read(int x, int y) const { return m_data[x + (y * m_width)]; }
+	void Write(int x, int y, const T& data) { m_data[x + (y * m_width)] = data; }
+
+
+	int m_height{ 0 };
+	int m_width{ 0 };
+
+	std::vector<T> m_data;
+};
